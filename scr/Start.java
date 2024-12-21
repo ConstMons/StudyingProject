00db0a8981a9e2b18d9ae7f07b71aea2d052b9d7
Mons <monstrosity372@gmail.com>
2024-12-22 03:45:57 +0800

    Test2

---
+++ scr/Start.java
@@ -0,0 +1,9 @@
+package scr;
+
+public class Start {
+    public static void main(String[] args) {
+        TextTest objA = new TextTest();
+
+        objA.helloworld();
+    }
+}
---
+++ scr/TextTest.java
@@ -0,0 +1,8 @@
+package scr;
+
+
+public class TextTest {
+    public void helloworld() {
+        System.out.println("Hello World");
+    }
+}
